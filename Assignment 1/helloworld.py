306aa4c1ef58a8c69feaadc14601032a174d03bb
DustinElliott <112398121+DustinElliott@users.noreply.github.com>
2022-09-08 08:18:16 -0400

    Update helloworld.py

--- Assignment 1/helloworld.py
+++ Assignment 1/helloworld.py
@@ -1,16 +1,14 @@
-#instructions: print out "Hello World!"
-#create a variable to hold your favorite color, then print out that variable with a string beforehand that says "My favorite color is: "
-#create a variable to hold your birthday (NO SLASHES, JUST THE DIGITS) and then print out "My birthday is: " with the variable
-
+# instructions: print out "Hello World!"
+# create a variable to hold your favorite color, then print out that variable with a string beforehand that says "My favorite color is: "
+# create a variable to hold your birthday (NO SLASHES, JUST THE DIGITS) and then print out "My birthday is: " with the variable
 
 
 def main():
+    print("Hello World!")
+    CLR = "blue"
+    print("My favorite color is: " +str (CLR))
+    BD = "3 18 2004"
+    print("My birthday is: " +str (BD))
 
 
-
-    
-    
-    
-    
-    
 main()
