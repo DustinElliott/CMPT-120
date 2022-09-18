a32c81bfac3d2c9e63ec08194fc46e76305d2f15
DustinElliott <112398121+DustinElliott@users.noreply.github.com>
2022-09-18 15:30:32 -0400

    Update if_else_input_practice.py

--- Assignment 2/if_else_input_practice.py
+++ Assignment 2/if_else_input_practice.py
@@ -6,8 +6,24 @@
 #The third if/else should read: Else if 10 is equal to 10, print out "10 is equal to 10"
 #The else should read: Else print out "10 is less than 10"
 
+Ten = 10
+if Ten > 12:
+    print("10 is greater than 12")
 
+elif Ten > 11:
+    print("10 is greater than 11")
+elif Ten == 10:
+    print("10 is equal to 10")
+else:
+    print("10 is less than 10")
 #additional instructions: instead of setting our variable to 10, what if we asked the user for the variable and used that value in our comparisons?
+
+age = input("How old are you? ")
+val = int(age)
+if (val > 10):
+    print(age, "is greater than 10")
+else:
+    print(age, "is less than or equal too 10")
 def main():
 
-main()
+    main()
